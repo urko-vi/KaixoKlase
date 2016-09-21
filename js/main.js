1c04610a63bd1918e238c804991d8a8b8d47080f
urko villanueva <urko.informatica@gmail.com>
2016-09-21 18:33:57 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*
+  @author: Ander Uraga Real
+  @version: 1.0
+  @date: 2016/09/19
+  Nuestro primer JavaScript
+*/
+
+
+//alert desaconsejado
+// alert("Kaixo gela");
+
+console.error("Muestra un mensaje de error");
+console.warn("Muestra un mensaje de Aviso");
+console.info("Muestra un mensaje Informativo");
+console.log("Muestra un mensaje de traza");
